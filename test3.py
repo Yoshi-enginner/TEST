2f8d543e0e0b07efb2d9f52c27263e07a7f7c2ea
Yoshiaki Shima <43871157+Yoshiaki-Shima@users.noreply.github.com>
2023-02-27 05:26:22 +0900

    Add files via upload

---
+++ test3.py
@@ -0,0 +1,6 @@
+import numpy as np 
+import pandas as pd # CSV file(pd.read_csv)
+import cv2
+import matplotlib.pyplot as plt
+df = pd.read_csv(r"C:\Users\tokyo\OneDrive\Documents\airbnb.csv")
+print(df)
